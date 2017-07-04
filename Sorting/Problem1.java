4ce4041d09c3fa29e4fe5b64d69b18f3f4dbc603
Nikhil Arora <narora200@gmail.com>
2017-07-04 13:25:55 +0530

    Problem 1 (Sorting): Comments added in Sorting/Problem1.java

--- Sorting/Problem1.java
+++ Sorting/Problem1.java
@@ -8,6 +8,7 @@ import java.util.*;
 
 public class Problem1 {
 
+    //function used to quicken up the quick-sort algorithm by randomly selecting a pivot
     static int randomisedPartition(int a[], int startIndex, int endIndex){
 
         int pivotIndex = (int) (startIndex + (Math.random()*10)%((endIndex-startIndex)+1));
@@ -19,6 +20,8 @@ public class Problem1 {
         int returnedPivotIndex = partition(a, startIndex, endIndex);
         return returnedPivotIndex;
     }
+
+    //function used to find out the pivot index
     static int partition(int a[], int startIndex, int endIndex){
 
         int pivotIndex = startIndex;
@@ -40,6 +43,8 @@ public class Problem1 {
         a[endIndex] = temp;
         return pivotIndex;
     }
+
+    //quick-sort algorithm
     static void quickSort(int a[], int startIndex, int endIndex){
 
         if(startIndex < endIndex){
@@ -49,13 +54,16 @@ public class Problem1 {
             quickSort(a, pivotIndex + 1, endIndex);
         }
     }
+
+    //function used for sorting the arrays in the 2D array
     static void sortArrays(int a[][]){
 
         for(int i=0; i<a.length; i++){
 
+            //instantiate a new array to fill in with the column of ith row
             int[] array = new int[a[i].length];
 
-            //get the array
+            //get the array in a variable
             for(int j=0; j<array.length; j++){
                 array[j] = a[i][j];
             }
@@ -64,7 +72,7 @@ public class Problem1 {
             quickSort(array, 0, array.length-1);
 
 
-            //put it right back where it belonged
+            //put it right back to where it belonged
             for(int j=0; j<array.length; j++){
                 a[i][j] = array[j];
             }
@@ -72,12 +80,15 @@ public class Problem1 {
 
 
         }
+
         //print the result
         printArrays(a);
 
 
     }
 
+
+    //dummy function used to individually print the 1D array (only for testing purpose)
     static void printArray(int a[]){
         for(int i=0; i<a.length; i++){
 
@@ -88,6 +99,8 @@ public class Problem1 {
         System.out.println();
     }
 
+
+    //function used to print the 2D array in the row-wise format
     static void printArrays(int a[][]){
         for(int i=0; i<a.length; i++){
             for(int j=0; j<a[i].length; j++){
@@ -99,21 +112,30 @@ public class Problem1 {
     public static void main(String args[]){
 
         Scanner scanner = new Scanner(System.in);
+
+        //get the number of test-cases in variable T
         int T = scanner.nextInt();
 
+        //instantiate only the first dimension of the array i.e. the number of rows as it is all that we know
         int[][] arrays = new int[T][];
-        //get the size of the arrays
+
+        //get the arrays
         for(int i=0; i<T; i++){
+
+            //get the size of the array
             int size = scanner.nextInt();
+
+            //instantiate the skewed 2D array with the column dimension
             arrays[i] = new int[size];
 
 
-
+            //get the values of the array and put it in the second dimension of the 2D array
             for(int j=0; j<arrays[i].length; j++){
                 arrays[i][j] = scanner.nextInt();
             }
         }
 
+        //sort the arrays
         sortArrays(arrays);
 
 
