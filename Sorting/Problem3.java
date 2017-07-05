d1a8bcef46943267e13eceb1e54735827b0f4135
Nikhil Arora <narora200@gmail.com>
2017-07-05 20:24:33 +0530

    Problem 3 (Sorting): Working code with System.out.print (Part 4)

--- Sorting/Problem3.java
+++ Sorting/Problem3.java
@@ -9,18 +9,79 @@ import java.util.*;
  */
 public class Problem3 {
 
+    //dummy function used to individually print the 1D array (only for testing purpose)
+    static void printArray(int a[]){
+        for(int i=0; i<a.length; i++){
+
+            System.out.print(a[i] + " ");
+
+
+        }
+        System.out.println();
+    }
+
     static void mergeArrays(int[][] collectionArray){
         //the 2 rows of the collectionArray have the 2 arrays that need to be merged
 
+        int sortedSize = collectionArray[0].length + collectionArray[1].length;
+        System.out.println("sortedArraysize: " + sortedSize);
+        int[] sortedArray = new int[sortedSize];
 
+        int[] firstArray = new int[collectionArray[0].length];
+        int[] secondArray = new int[collectionArray[1].length];
 
-        for(int i=0; i<collectionArray.length; i++){
-
-            for(int j=0; j<collectionArray[i].length; j++){
-                System.out.print(collectionArray[i][j] + " ");
-            }
-            System.out.println();
+        for(int l=0; l<collectionArray[0].length; l++){
+            firstArray[l] = collectionArray[0][l];
         }
+        printArray(firstArray);
+        for(int m=0; m<collectionArray[1].length; m++){
+            secondArray[m] = collectionArray[1][m];
+        }
+        printArray(secondArray);
+        int i = 0, j = 0, k = 0;
+        while(i < firstArray.length && j<secondArray.length){
+
+
+
+
+                if(firstArray[i] > secondArray[j]){
+                    sortedArray[k] = firstArray[i];
+                    System.out.println("firstArray[i] is: " + firstArray[i]);
+                    i++;
+                }else{
+                    sortedArray[k] = secondArray[j];
+                    j++;
+                }
+                System.out.println("sortedArray: " + sortedArray[k]);
+                k++;
+
+
+
+        }
+
+
+        while(i < firstArray.length){
+
+            sortedArray[k] = firstArray[i];
+            System.out.println("sortedArray: " + sortedArray[k]);
+            i++;
+            k++;
+        }
+
+        while(j < secondArray.length){
+
+            sortedArray[k] = secondArray[j];
+            System.out.println("sortedArray: " + sortedArray[k]);
+            j++;
+            k++;
+        }
+
+        for(int iterator=0; iterator<sortedArray.length; iterator++){
+
+            System.out.print(sortedArray[iterator] + " ");
+
+        }
+        System.out.println();
     }
 
     static void printIntegerArrays(int[][] collectionArray){
@@ -61,9 +122,9 @@ public class Problem3 {
         }
 
 
-        mergeArrays(collectionArray);
-        printIntegerArrays(collectionArray);
 
+        printIntegerArrays(collectionArray);
+        mergeArrays(collectionArray);
 
     }
     static void printArrays(String[][] stringArray){
