fc3c5983e98353b4c16476eb87cf71c7d40ae7c8
Nikhil Arora <narora200@gmail.com>
2017-07-06 22:23:21 +0530

    Problem 4 (Sorting): Input taken and stored (Part 1)

---
+++ Sorting/Problem4.java
@@ -0,0 +1,52 @@
+package com.iamoperand.com.hackerearth.iamoperand.Sorting;
+
+/**
+ * Created by iamoperand on 6/7/17.
+ */
+
+import java.util.*;
+public class Problem4 {
+
+    static void printMatrixString(String[] matrixString){
+
+        for(int i=0; i<matrixString.length; i++){
+
+            System.out.println(matrixString[i]);
+        }
+    }
+    public static void main(String args[]){
+
+        Scanner scanner = new Scanner(System.in);
+        int T  = scanner.nextInt();
+
+        int[] curseArray = new int[T];
+        String[] theMatrix = new String[T];
+
+        for(int i=0; i<T; i++){
+            //get the value of N
+            int N = scanner.nextInt();
+
+            //get the values filled in curseArray one-by-one
+            curseArray[i] = scanner.nextInt();
+
+
+            //fill in the values of poison content, N^2 times.
+            String matrixString = "";
+
+            for(int j=0; j < Math.pow(N, 2); j++){
+                matrixString += scanner.nextInt() + " ";
+            }
+
+            //got the values into the matrixString. Now put it inside theMatrix.
+
+            theMatrix[i] = matrixString;
+
+        }
+
+
+        printMatrixString(theMatrix);
+    }
+
+
+
+}
