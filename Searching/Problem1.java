908d8d90d41b4f9f5d4a0496ffcfb5b1cce22149
Nikhil Arora <narora200@gmail.com>
2017-07-07 12:28:49 +0530

    Problem 1 (Searching): Complete working code (Part 2)

--- Searching/Problem1.java
+++ Searching/Problem1.java
@@ -9,6 +9,23 @@ public class Problem1 {
 
     static void mainLogic(int[] actualArray, int[] queryArray){
 
+        for(int i=0; i<queryArray.length; i++){
+            int flag = 0;
+            for(int j=0; j<actualArray.length; j++){
+
+                if(queryArray[i] == actualArray[j]){
+                   System.out.println("YES");
+                    flag++;
+                    break;
+               }
+
+
+            }
+            if(flag == 0){
+                System.out.println("NO");
+            }
+
+        }
 
     }
     public static void main(String args[]){
