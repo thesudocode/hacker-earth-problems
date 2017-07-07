d2ba936e4f9c91f0f85507cebc97294bd173c6e3
Nikhil Arora <narora200@gmail.com>
2017-07-07 19:26:01 +0530

    Problem 4 (Searching): Inputs taken and stored (Part 1)

---
+++ Searching/Problem4.java
@@ -0,0 +1,22 @@
+package com.iamoperand.com.hackerearth.iamoperand.Searching;
+
+import java.util.Scanner;
+
+/**
+ * Created by iamoperand on 7/7/17.
+ */
+public class Problem4 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+
+        int[] templePositions = new int[N];
+
+        for(int i=0; i<templePositions.length; i++){
+            templePositions[i] = scanner.nextInt();
+        }
+
+
+    }
+}
